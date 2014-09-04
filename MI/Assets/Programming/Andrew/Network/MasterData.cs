fa71763f7e52e64a7b22f359bb3406fa4223d740
AndrewXA9 <andrewxa9@yahoo.com>
2014-09-03 18:28:21 -0700

    added network core

---
+++ MI/Assets/Programming/Andrew/Network/MasterData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterData : MonoBehaviour{
+	
+	public List<Player> Players = new List<Player>();
+	
+	public static MasterData MasterDataInstance;
+	
+	public void registerPlayer(int _playerNumber, NetworkViewID _NVID){
+		networkView.RPC("RegisterPlayer",RPCMode.Server,_playerNumber,_NVID);
+	}
+	
+	
+	[RPC]
+	void RegisterPlayer(int _playerNumber, NetworkViewID _NVID);
+}
+
+public class Player{
+	int playerNumber;
+	NetworkViewID NVID;
+	
+	Player(int _playerNumber, NetworkViewID _NVID){
+		playerNumber = _playerNumber;
+		NVID = _NVID;
+	}
+}
---
+++ MI/Assets/Programming/Andrew/Network/NetworkLobby.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkLobby : MonoBehaviour {
+
+	private HostData[] hostList;
+	public string ip = "127.0.0.1";
+	public int port = 8080;
+	
+	private bool isHost = false;
+	
+	private string gameName;
+	
+	public string MSIP = "smp.awesomecraft.net";
+	
+	//SERVER SPECIFIC
+	
+	private List<NetworkViewID> IDlist;
+	
+	
+	void Start(){
+		//MasterServer.ipAddress = MSIP;
+		gameName = "Game "+((int)Random.Range(1000,9999)).ToString();
+		RefreshHostList();
+		//Network.TestConnection();
+	}
+	
+	void Update(){
+		
+	}
+	
+	void RefreshHostList(){
+		MasterServer.ClearHostList();
+		MasterServer.RequestHostList("Sol");
+	}
+	
+	void OnGUI(){
+		
+		if(GUI.Button(new Rect(50,Screen.height-100,200,50),"Refresh")){
+			RefreshHostList();
+		}
+		
+		if (!isHost){
+			gameName = GUI.TextField(new Rect(250,Screen.height-150,200,50),gameName);
+			if(GUI.Button(new Rect(250,Screen.height-100,200,50),"Register")){
+				//Network.InitializeServer(32, port, false);
+				Network.InitializeServer(32, port+(int)Random.Range(1,100), false);
+				MasterServer.RegisterHost("Sol",gameName);
+				RefreshHostList();
+				isHost = true;
+			}
+		}
+		
+		if(MasterServer.PollHostList().Length != 0){
+			for(int i=0;i<MasterServer.PollHostList().Length;i++){
+				GUI.Label(new Rect(50,50+(i*50),50,25),i.ToString());
+				GUI.Label(new Rect(100,50+(i*50),200,25),MasterServer.PollHostList()[i].gameName);
+				GUI.Button(new Rect(250,50+(i*50),50,25),"Join");
+			}
+		}
+	}
+}
