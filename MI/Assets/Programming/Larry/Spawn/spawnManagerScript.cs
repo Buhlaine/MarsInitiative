c3d3891dd7b71ce96522e2c741a4a672dbfdbb78
infi17yte33 <saeyangl@gmail.com>
2014-09-03 20:57:37 -0700

    added start spawn scripts

--- MI/Assets/Programming/Larry/Spawn/spawnManagerScript.cs
+++ MI/Assets/Programming/Larry/Spawn/spawnManagerScript.cs
@@ -5,32 +5,46 @@ using System.Collections.Generic;
 public class spawnManagerScript : MonoBehaviour 
 {
 	Dictionary<GameObject, int> allSpawns = new Dictionary<GameObject, int> ();
+	List<GameObject> blueStart = new List<GameObject>();
+	List<GameObject> redStart = new List<GameObject>();
 	GameObject[] spawnPoints;
 	List<GameObject> possibleSpawns = new List<GameObject> ();
-	public GameObject playerGO;
 	GameObject finalSpawnPos;
 	int spawnWeight = 1000;
+
+
+	//testing----------------------------------------------
+	GameObject thisGO;
+	//----------------------------------------------------
+	public GameObject scout;
+	public GameObject heavy;
+	public GameObject trooper;
+	bool gameBegin = false;
 
 	//Testing stuff
 	//int playerNumber = 5;
 	
 	void Start () 
 	{
-//Testing start spawn----------------------------------------------------------------
-//		GameObject startSpawn = GameObject.FindGameObjectWithTag ("startSpawn");
-//		bool spawned = false;
-//		Vector2 startSpawns = new Vector2();
-//		for(int i = 0; i<playerNumber; i++)
-//		{
-//			while(!spawned)
-//			{
-//				startSpawn = Random.insideUnitCircle * 20;
-//			}
-//		}
-		//------------------------------------------------------------------------------------
+		GameObject[] allStartSpawn = GameObject.FindGameObjectsWithTag ("startSpawns");
+
+		foreach(GameObject spawnStart in allStartSpawn)
+		{
+			if(spawnStart.gameObject.name.Contains("Blue"))
+			{
+				blueStart.Add(spawnStart);
+			}
+			else if(spawnStart.gameObject.name.Contains("Red"))
+			{
+				redStart.Add(spawnStart);
+			}
+		}
+
+		blueStart.TrimExcess ();
+		redStart.TrimExcess ();
 
 		//Aquiring all spawnPoints in the scene
-		spawnPoints = GameObject.FindGameObjectsWithTag ("spawnPoints");
+		spawnPoints = GameObject.FindGameObjectsWithTag ("spawnPoint");
 
 		//Putting spawns in a dictionary to associate spawn with its weight for choosing spawnPoint when spawn is requested
 		foreach(GameObject GO in spawnPoints)
@@ -46,26 +60,42 @@ public class spawnManagerScript : MonoBehaviour
 		//Player request to spawn
 		if(Input.GetKeyDown(KeyCode.Q))
 		{
-			checkSpawn(playerGO);
+			for(int i = 0; i < 8; i++)
+			{
+				Debug.Log(i);
+				checkSpawn(thisGO);
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			checkSpawn(thisGO);
+		}
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			setGameBegin();
 		}
 	}
 
 	void checkSpawn(GameObject _requestor)
 	{
-		//_requestor.tag = "Blue";
-
-		//grabbing playerDetect script to access it's spawnWeight
-		//Might change this to recieve an array to avoid having to access script directly
-		playerDetect spawnWeight;
-		//iterating through all spawns
-		foreach(GameObject spawnP in spawnPoints)
+		Debug.Log (gameBegin);
+		if(gameBegin)
 		{
-			//requesting all spawnPoints to calculate their spawnWeights
-			spawnP.SendMessage("calcSpawnWeight", _requestor.tag);
-			//accessing their script to get spawn weight
-			spawnWeight = spawnP.GetComponent<playerDetect>();
-			//setting their spawnWeight in the Dictionary
-			allSpawns[spawnP] = spawnWeight.getSpawnWeight();
+			Debug.Log("--------------ljksdhfjhsdfjkhdflkjslkjf");
+			//_requestor.tag = "Red";
+			//grabbing playerDetect script to access it's spawnWeight
+			//Might change this to recieve an array to avoid having to access script directly
+			playerDetect spawnWeight;
+			//iterating through all spawns
+			foreach(GameObject spawnP in spawnPoints)
+			{
+				//requesting all spawnPoints to calculate their spawnWeights
+				spawnP.SendMessage("calcSpawnWeight", "Red"/*_requestor.tag*/);
+				//accessing their script to get spawn weight
+				spawnWeight = spawnP.GetComponent<playerDetect>();
+				//setting their spawnWeight in the Dictionary
+				allSpawns[spawnP] = spawnWeight.getSpawnWeight();
+			}
 		}
 		//running spawn
 		spawn (_requestor);
@@ -73,141 +103,211 @@ public class spawnManagerScript : MonoBehaviour
 
 	void spawn(GameObject playerSpawning)
 	{
-		//used if multiple spawn points have the same weight
-		bool multiple = false;
-		//int posSpawncount = 0;
+		//For final-------------------------------------------------------------------
+		//move playerScript = playerSpawning.GetComponent<move> ();
+		//string playerClass = playerScript.playerClass;
+		//----------------------------------------------------------------------------
 
-		//going through all spawnPoints in array spawnPoints
-		foreach(GameObject GO in spawnPoints)
+		//for testing---------------------------------------------------------------
+		string tag = "Red";
+		string playerClass = "scout";
+		//-----------------------------------------------------------------------------
+		if(!gameBegin)
 		{
-			//if multiple spawnPoints have the same weight
-			if(multiple)
+			//if(playerSpawning.tag == "Blue")
+			//testing-------------------------------------------
+			if(tag == "Blue")
 			{
-				//if current spawnPoint, GO, has a higher spawnWeight
-				if(allSpawns[GO] > allSpawns[finalSpawnPos])
+				if(playerClass == "scout")
 				{
-					//the finalSpawn is changed to GO
-					finalSpawnPos = GO;
-					//multiple is set to false
-					multiple = false;
-					//posSpawnPoints is cleared
-					possibleSpawns.Clear();
+					GameObject.Instantiate (scout, blueStart[0].transform.position, Quaternion.identity);
 				}
-				//if current spawn has same weight
-				else if(allSpawns[GO] == allSpawns[finalSpawnPos])
+				else if(playerClass == "trooper")
 				{
-					//add to list possibleSpawns
-					possibleSpawns.Add(GO);
-					//posSpawncount++;
+					GameObject.Instantiate (trooper, blueStart[0].transform.position, Quaternion.identity);
 				}
+				else if(playerClass == "heavy")
+				{
+					GameObject.Instantiate (heavy, blueStart[0].transform.position, Quaternion.identity);
+				}
+				blueStart.RemoveAt(0);
 			}
-			//if multiple is false
-			else
+			//else if(playerSpawning.tag == "Red")
+			//testing------------------------------------
+			else if(tag == "Red")
 			{
-				//if finalSpawn isn't set
-				if(finalSpawnPos == null)
+				if(playerClass == "scout")
 				{
-					//finalSpawn is set to current spawn
-					finalSpawnPos = GO;
+					GameObject.Instantiate (scout, redStart[0].transform.position, Quaternion.identity);
 				}
-				//if finalSpawn is set
+				else if(playerClass == "trooper")
+				{
+					GameObject.Instantiate (trooper, redStart[0].transform.position, Quaternion.identity);
+				}
+				else if(playerClass == "heavy")
+				{
+					GameObject.Instantiate (heavy, redStart[0].transform.position, Quaternion.identity);
+				}
+
+				redStart.RemoveAt(0);
+			}
+		}
+		else if(gameBegin)
+		{
+			//used if multiple spawn points have the same weight
+			bool multiple = false;
+			//int posSpawncount = 0;
+
+			//going through all spawnPoints in array spawnPoints
+			foreach(GameObject GO in spawnPoints)
+			{
+				//if multiple spawnPoints have the same weight
+				if(multiple)
+				{
+					//if current spawnPoint, GO, has a higher spawnWeight
+					if(allSpawns[GO] > allSpawns[finalSpawnPos])
+					{
+						//the finalSpawn is changed to GO
+						finalSpawnPos = GO;
+						//multiple is set to false
+						multiple = false;
+						//posSpawnPoints is cleared
+						possibleSpawns.Clear();
+					}
+					//if current spawn has same weight
+					else if(allSpawns[GO] == allSpawns[finalSpawnPos])
+					{
+						//add to list possibleSpawns
+						possibleSpawns.Add(GO);
+						//posSpawncount++;
+					}
+				}
+				//if multiple is false
 				else
 				{
-					//if currentSpawn has same weight as finalSpawn and finalSpawn isn't the same object as currentSpawn
-					if(allSpawns[GO] == allSpawns[finalSpawnPos] && finalSpawnPos != GO)
+					//if finalSpawn isn't set
+					if(finalSpawnPos == null)
 					{
-						//set multiple to true
-						multiple = true;
-						//Adds finalSpawn to possibleSpawns
-						possibleSpawns.Add(finalSpawnPos);
-						//Adds currentSpawn to possibleSpawns
-						possibleSpawns.Add(GO);
-					}
-					//if currentSpawns spawnWeight is greater
-					else if(allSpawns[GO] > allSpawns[finalSpawnPos])
-					{
-						//finalSpawn is set to currentSpawn
+						//finalSpawn is set to current spawn
 						finalSpawnPos = GO;
+					}
+					//if finalSpawn is set
+					else
+					{
+						//if currentSpawn has same weight as finalSpawn and finalSpawn isn't the same object as currentSpawn
+						if(allSpawns[GO] == allSpawns[finalSpawnPos] && finalSpawnPos != GO)
+						{
+							//set multiple to true
+							multiple = true;
+							//Adds finalSpawn to possibleSpawns
+							possibleSpawns.Add(finalSpawnPos);
+							//Adds currentSpawn to possibleSpawns
+							possibleSpawns.Add(GO);
+						}
+						//if currentSpawns spawnWeight is greater
+						else if(allSpawns[GO] > allSpawns[finalSpawnPos])
+						{
+							//finalSpawn is set to currentSpawn
+							finalSpawnPos = GO;
+						}
 					}
 				}
 			}
-		}
-//-----------------------------------------------------------------------------------------------------------------------
-		//For some reason I didnt think to use foreach
-		//Dumb me, fixed it up top. Leaving this part in for now just in case top doesn't work right
-//		for(int i = 0; i < spawnPoints.Length; i++)
-//		{
-//			if(multiple)
-//			{
-//				if(allSpawns[spawnPoints[i]] > allSpawns[finalSpawnPos])
-//				{
-//					finalSpawnPos = spawnPoints[i];
-//					multiple = false;
-//					//posSpawncount = 0;
-//				}
-//				else if(allSpawns[spawnPoints[i]] == allSpawns[finalSpawnPos])
-//				{
-//					possibleSpawns.Add(spawnPoints[i]);
-//					//posSpawncount++;
-//				}
-//			}
-//			else
-//			{
-//				if(finalSpawnPos == null)
-//				{
-//					finalSpawnPos = spawnPoints[i];
-//				}
-//				else
-//				{
-//					if(allSpawns[spawnPoints[i]] == allSpawns[finalSpawnPos])
-//					{
-//						multiple = true;
-//						//possibleSpawns = new GameObject[8];
-//						possibleSpawns.Add(finalSpawnPos);
-//						possibleSpawns.Add(spawnPoints[i]);
-//						//posSpawncount = 2;
-//					}
-//					else if(allSpawns[spawnPoints[i]] > allSpawns[finalSpawnPos])
-//					{
-//						finalSpawnPos = spawnPoints[i];
-//					}
-//				}
-//			}
-//		}
-//---------------------------------------------------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------------------------------------
+			//For some reason I didnt think to use foreach
+			//Dumb me, fixed it up top. Leaving this part in for now just in case top doesn't work right
+	//		for(int i = 0; i < spawnPoints.Length; i++)
+	//		{
+	//			if(multiple)
+	//			{
+	//				if(allSpawns[spawnPoints[i]] > allSpawns[finalSpawnPos])
+	//				{
+	//					finalSpawnPos = spawnPoints[i];
+	//					multiple = false;
+	//					//posSpawncount = 0;
+	//				}
+	//				else if(allSpawns[spawnPoints[i]] == allSpawns[finalSpawnPos])
+	//				{
+	//					possibleSpawns.Add(spawnPoints[i]);
+	//					//posSpawncount++;
+	//				}
+	//			}
+	//			else
+	//			{
+	//				if(finalSpawnPos == null)
+	//				{
+	//					finalSpawnPos = spawnPoints[i];
+	//				}
+	//				else
+	//				{
+	//					if(allSpawns[spawnPoints[i]] == allSpawns[finalSpawnPos])
+	//					{
+	//						multiple = true;
+	//						//possibleSpawns = new GameObject[8];
+	//						possibleSpawns.Add(finalSpawnPos);
+	//						possibleSpawns.Add(spawnPoints[i]);
+	//						//posSpawncount = 2;
+	//					}
+	//					else if(allSpawns[spawnPoints[i]] > allSpawns[finalSpawnPos])
+	//					{
+	//						finalSpawnPos = spawnPoints[i];
+	//					}
+	//				}
+	//			}
+	//		}
+	//---------------------------------------------------------------------------------------------------------------------------
 
-		//if multiple is true
-		if(multiple)
-		{
-			//Make sure possibleSpawns count is accurate
-			possibleSpawns.TrimExcess();
-			//Get a random number to choose spawnPoint randomly
-			int randNum = Random.Range(0, possibleSpawns.Count - 1);
-			//Stand-in for actual spawning. Need to add check for different classes
-			GameObject.Instantiate (playerSpawning, possibleSpawns[randNum].transform.position, Quaternion.identity);
-			//Goes through each spawnPoint and request them to resetspawn so weight is accurate next time spawn is requested
-			foreach(GameObject GO in spawnPoints)
+			//if multiple is true
+			if(multiple)
 			{
-				GO.SendMessage("resetWeight");
+				//Make sure possibleSpawns count is accurate
+				possibleSpawns.TrimExcess();
+				//Get a random number to choose spawnPoint randomly
+				int randNum = Random.Range(0, possibleSpawns.Count - 1);
+				//Stand-in for actual spawning. Need to add check for different classes
+				if(playerClass == "scout")
+					GameObject.Instantiate (scout, possibleSpawns[randNum].transform.position, Quaternion.identity);
+				else if(playerClass == "")
+					GameObject.Instantiate (trooper, possibleSpawns[randNum].transform.position, Quaternion.identity);
+				else if(playerClass == "heavy")
+					GameObject.Instantiate (heavy, possibleSpawns[randNum].transform.position, Quaternion.identity);
+				//Goes through each spawnPoint and request them to resetspawn so weight is accurate next time spawn is requested
+				foreach(GameObject GO in spawnPoints)
+				{
+					GO.SendMessage("resetWeight");
+				}
+				//Clear possibleSpawns so old spawns aren't included in new calculation
+				possibleSpawns.Clear();
+				//Make sure multiple is set to false
+
+				multiple = false;
 			}
-			//Clear possibleSpawns so old spawns aren't included in new calculation
-			possibleSpawns.Clear();
-			//Make sure multiple is set to false
-			multiple = false;
-		}
-		else
-		{
-			//Stand-in for actual spawning. Need to add check for different classes
-			GameObject.Instantiate (playerSpawning, finalSpawnPos.transform.position, Quaternion.identity);
-			//Goes through each spawnPoint and request them to resetspawn so weight is accurate next time spawn is requested
-			foreach(GameObject GO in spawnPoints)
+			else
 			{
-				GO.SendMessage("resetWeight");
+				//Stand-in for actual spawning. Need to add check for different classes
+				if(playerClass == "scout")
+					GameObject.Instantiate (scout, finalSpawnPos.transform.position, Quaternion.identity);
+				else if(playerClass == "trooper")
+					GameObject.Instantiate (trooper, finalSpawnPos.transform.position, Quaternion.identity);
+				else if(playerClass == "heavy")
+					GameObject.Instantiate (heavy, finalSpawnPos.transform.position, Quaternion.identity);
+
+				//Goes through each spawnPoint and request them to resetspawn so weight is accurate next time spawn is requested
+				foreach(GameObject GO in spawnPoints)
+				{
+					GO.SendMessage("resetWeight");
+				}
+				//Clear possibleSpawns so old spawns aren't included in new calculation
+				possibleSpawns.Clear();
+				//Make sure multiple is set to false
+				multiple = false;
 			}
-			//Clear possibleSpawns so old spawns aren't included in new calculation
-			possibleSpawns.Clear();
-			//Make sure multiple is set to false
-			multiple = false;
 		}
+	}
+
+	void setGameBegin()
+	{
+		Debug.Log ("++++++++++++++++++++++++++ljkhsdfkhsdfojfdjsoidjoifjds");
+		gameBegin = !gameBegin;
 	}
 }
