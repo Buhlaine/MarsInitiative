8f874d5fa5ec5092dbc7b08222318fb63757219e
andrewxa9 <andrewxa9@yahoo.com>
2014-10-15 21:48:59 -0700

    network

--- MI/Assets/Programming/Andrew/Network/MasterData.cs
+++ MI/Assets/Programming/Andrew/Network/MasterData.cs
@@ -4,26 +4,66 @@ using System.Collections.Generic;
 
 public class MasterData : MonoBehaviour{
 	
-	public List<Player> Players = new List<Player>();
+	public List<NetworkPlayerModule> Players = new List<NetworkPlayerModule>();
 	
 	public static MasterData MasterDataInstance;
 	
-	public void registerPlayer(int _playerNumber, NetworkViewID _NVID){
-		networkView.RPC("RegisterPlayer",RPCMode.Server,_playerNumber,_NVID);
+	public static Player me;
+	
+	private int responses;
+	
+	void Start(){
+		MasterDataInstance = this;
 	}
 	
+	//Client Locals
+	public void sendRegisterRequest(NetworkViewID _NVID){
+		networkView.RPC("registerPlayer",RPCMode.Server, this.networkView.viewID);
+	}
+	
+	
+	//Server Locals
+	public IEnumerator Waitresponses(){
+		responses = 0;
+		networkView.RPC("sendPlayerCount",RPCMode.All,Players.Count);
+		while(responses < Players.Count){
+			yield return null;
+		}
+		foreach(NetworkPlayerModule i in Players){
+			networkView.RPC("sendPlayerInfo",RPCMode.All,i.playerNumber,i.NVID);
+		}
+	}
+	
+	//Client RPCs
+	[RPC]
+	void sendPlayerCount(int _number){
+		networkView.RPC("incrementResponse",RPCMode.Server);
+	}
+	
+	
+	//Server RPC's
+	[RPC]
+	void registerPlayer(NetworkViewID _NVID){
+		Players.Add(new NetworkPlayerModule(Players.Count+1,_NVID));
+	}
 	
 	[RPC]
-	void RegisterPlayer(int _playerNumber, NetworkViewID _NVID){
-
+	void incrementResponse(){
+		responses++;
 	}
+	
+	[RPC]
+	void requestPlayerList(){
+		
+	}
+	
 }
 
 public class NetworkPlayerModule{
-	int playerNumber;
-	NetworkViewID NVID;
+	public int playerNumber;
+	public NetworkViewID NVID;
 	
-	NetworkPlayerModule(int _playerNumber, NetworkViewID _NVID){
+	public NetworkPlayerModule(int _playerNumber, NetworkViewID _NVID){
 		playerNumber = _playerNumber;
 		NVID = _NVID;
 	}
