4e08fb97111217d42749bd1f0870f7b748a61001
dorichan <dori.p.chan@gmail.com>
2014-10-29 19:30:14 -0700

    -bug fixes in Enforcer-Tackle, Assassin-ChainShot/Cloak -added point tracker

---
+++ MI/Assets/Programming/Dori/Scripts/PointTracker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Script that keeps track of how many points each team has from
+ * flag captures. */
+
+public class PointTracker : MonoBehaviour 
+{
+	public int redTeamPoints;
+	public int blueTeamPoints;
+	public int capturePoints;
+
+	public bool addingPoints;
+
+	void Start()
+	{
+		redTeamPoints = 0;
+		blueTeamPoints = 0;
+
+		capturePoints = 1;
+	}
+
+	public void AddPoints(string _team)
+	{
+		// Will this keep it from happening more than once?
+		if(_team == "Blue" && !addingPoints) {
+			addingPoints = true;
+			blueTeamPoints += capturePoints;
+			addingPoints = false;
+			// TODO Send message to GUI to display points
+		}
+		if(_team == "Red" && !addingPoints) {
+			addingPoints = true;
+			redTeamPoints += capturePoints;
+			addingPoints = false;
+			// TODO Send message to GUI to display points
+		}
+	}
+}
