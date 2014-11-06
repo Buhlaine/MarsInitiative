7c9b96add7e2d56f9f15801b6a45e376787c4613
andrewxa9 <andrewxa9@yahoo.com>
2014-11-05 22:02:21 -0800

    netthing

---
+++ MI/Assets/Programming/Andrew/Network/PlayerNumber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNumber : MonoBehaviour {
+
+	public int number;
+	
+	void Start(){
+		number = int.TryParse(MasterData.me.ToString());
+	}
+
+}
