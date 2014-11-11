cb038a2a6ca9bf703a7309dd192f9c29d89b778e
marcusmriley <marcusmriley@gmail.com>
2014-11-11 15:21:58 -0800

    Networking for the animations

--- MI/Assets/Programming/Marcus/Weapons/Scripts/grenadeScript.cs
+++ MI/Assets/Programming/Marcus/Weapons/Scripts/grenadeScript.cs
@@ -13,7 +13,7 @@ public class grenadeScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetButtonDown("Fire3"))
+		if (Input.GetButtonDown("Fire2"))
 		{
 			if ( amountOfGrenades[statsLevel] > 0)
 			{
