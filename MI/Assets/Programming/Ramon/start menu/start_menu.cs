b978e65697a623aed641f1fb42513f477638c069
heymon <rcavalleiro@gmail.com>
2014-12-03 18:40:17 -0800

    start menu

--- MI/Assets/Programming/Ramon/start menu/start_menu.cs
+++ MI/Assets/Programming/Ramon/start menu/start_menu.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class start_menu : MonoBehaviour {
 
+	public float fontpercent;
 	public GUIStyle backlogo;
 	public GUIStyle frontlogo;
 	public GUIStyle[] button;
@@ -11,8 +12,10 @@ public class start_menu : MonoBehaviour {
 	public GUIStyle BN;
 	string[] description1 = new string[10];
 	int selection;
-	float[] widthpos=new float[7];
-	float[] heightpos=new float[7];
+	float[] widthpos=new float[11];
+	float[] heightpos=new float[11];
+	float[] widthdim=new float[5];
+	float[] heightdim=new float[5];
 	//float i = 2;
 	int type;
 	bool recoverOrstrike=false;
@@ -34,6 +37,11 @@ public class start_menu : MonoBehaviour {
 //		description1[4] = "Create: Start a lobby.";
 //		description1[5] = "Join: Join an existing lobby.";
 
+
+
+
+
+		//////////////////BUTTONS POSITION//////////////////////////////////////////////
 		widthpos [0] = 0.09375f;
 		heightpos [0] = 0.5288461f;
 		
@@ -49,19 +57,46 @@ public class start_menu : MonoBehaviour {
 		widthpos [4] = 0.4f;
 		heightpos [4] = 0.7f;
 
-		widthpos [5] = 0.65f;
+		widthpos [5] = 0.65f;//1040
 		heightpos [5] = 0.5288461f;
 
-		widthpos [6] = 0.65f;
-		heightpos [6] = 0.7f;
+		widthpos [6] = 0.759375f;//1215
+		heightpos [6] = 0.5288461f;//549.9999
+
+		widthpos [7] = 0.65f;
+		heightpos [7] = 0.5288461f;
+
+		widthpos [8] = 0.65f;
+		heightpos [8] = 0.7f;//728
+
+		widthpos [9] = 0.759375f;
+		heightpos [9] = 0.7f;//728
+
+		widthpos [10] = 0.65f;
+		heightpos [10] = 0.7f;//728
+		//////////////////BUTTONS POSITION//////////////////////////////////////////////
+		/// 
+		/// 
+		//////////////////BUTTONS DIMENSIONS//////////////////////////////////////////////
+		widthdim [0] = 0.21875f;//350
+		heightdim [0] = 0.1442308f;//150
+
+		widthdim [1] = 0.109375f;//175
+		heightdim [1] = 0.1442308f;//150
+		//////////////////BUTTONS DIMENSIONS//////////////////////////////////////////////
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		Debug.Log (0.65f*Screen.width);//1040
+		//Debug.Log (0.09375f*Screen.width);
 //		Debug.Log (0.5288461f*Screen.height);//549.9999
-		Debug.Log (0.7f*Screen.height);//728
+//		Debug.Log (0.7f*Screen.height);//728
+		//960f, 927f, 650f, 31f
+		//0.6f, 0.8913462f, 0.8913462f, 0.02980769f
+		//Debug.Log (((940f / Screen.width) * 100) / 100);//0.759375
+		//Debug.Log (((968f / Screen.height) * 100) / 100);//0.759375
+		fontpercent=((widthpos[0]*Screen.width) / 150);
 	
 	}
 
@@ -71,20 +106,23 @@ public class start_menu : MonoBehaviour {
 
 		GUI.Box (new Rect (0.0f, 0.0f, Screen.width, Screen.height), "", backlogo);
 
-		GUI.Box (new Rect (640.0f, 150.0f, 700f, 350f), "", frontlogo);
-
-
-		Rect Button1=new Rect (widthpos[0]*Screen.width, heightpos[0]*Screen.height, 350.0f, 150.0f);
-		Rect Button2=new Rect (widthpos[1]*Screen.width, heightpos[1]*Screen.height, 350.0f, 150.0f);
-		Rect Button3=new Rect (widthpos[2]*Screen.width, heightpos[2]*Screen.height, 350.0f, 150.0f);
-		Rect Button4=new Rect (widthpos[3]*Screen.width, heightpos[3]*Screen.height, 350.0f, 150.0f);
-		Rect Button5=new Rect (widthpos[4]*Screen.width, heightpos[4]*Screen.height, 350.0f, 150.0f);
-		Rect Button6=new Rect (widthpos[5]*Screen.width, heightpos[5]*Screen.height, 350.0f, 150.0f);
-		Rect Button6BlueHalf=new Rect (1215f, 550f, 175.0f, 150.0f);
-		Rect Button6RedHalf=new Rect (1040f, 550f, 175.0f, 150.0f);
-		Rect Button7=new Rect (widthpos[6]*Screen.width, heightpos[6]*Screen.height, 350.0f, 150.0f);
-		Rect Button7BlueHalf=new Rect (1215f, 728f, 175.0f, 150.0f);
-		Rect Button7RedHalf=new Rect (1040f, 728f, 175.0f, 150.0f);
+		//GUI.Box (new Rect (640.0f, 150.0f, 700f, 350f), "", frontlogo);
+		GUI.Box (new Rect (0.4f*Screen.width, 0.1442308f*Screen.height, 0.4375f*Screen.width, 0.3365385f*Screen.height), "", frontlogo);
+		//comment1=960f, 927f, 650f, 31f
+		Rect comment1 = new Rect (0.6f * Screen.width, 0.8913462f * Screen.height, 0.8913462f * Screen.width, 0.02980769f * Screen.height);
+		//comment2=940f, 968f, 650f, 31f
+		Rect comment2 = new Rect (0.5875f * Screen.width, 0.9307692f * Screen.height, 0.8913462f * Screen.width, 0.02980769f * Screen.height);
+		Rect Button1=new Rect (widthpos[0]*Screen.width, heightpos[0]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button2=new Rect (widthpos[1]*Screen.width, heightpos[1]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button3=new Rect (widthpos[2]*Screen.width, heightpos[2]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button4=new Rect (widthpos[3]*Screen.width, heightpos[3]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button5=new Rect (widthpos[4]*Screen.width, heightpos[4]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button6=new Rect (widthpos[5]*Screen.width, heightpos[5]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button6BlueHalf=new Rect (widthpos[6]*Screen.width, heightpos[6]*Screen.height, widthdim[1]*Screen.width, heightdim[1]*Screen.height);
+		Rect Button6RedHalf=new Rect (widthpos[7]*Screen.width, heightpos[7]*Screen.height, widthdim[1]*Screen.width, heightdim[1]*Screen.height);
+		Rect Button7=new Rect (widthpos[8]*Screen.width, heightpos[8]*Screen.height, widthdim[0]*Screen.width, heightdim[0]*Screen.height);
+		Rect Button7BlueHalf=new Rect (widthpos[9]*Screen.width, heightpos[9]*Screen.height, widthdim[1]*Screen.width, heightdim[1]*Screen.height);
+		Rect Button7RedHalf=new Rect (widthpos[10]*Screen.width, heightpos[10]*Screen.height, widthdim[1]*Screen.width, heightdim[1]*Screen.height);
 
 		if(selection==1)
 		{
@@ -230,57 +268,57 @@ public class start_menu : MonoBehaviour {
 
 		if(Button1.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[1], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[1], MD);
+			GUI.Box (comment2, description1[0], MD);
 		}
 		if(Button2.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[2], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[2], MD);
+			GUI.Box (comment2, description1[0], MD);
 		}
 		if(Button3.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[3], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[3], MD);
+			GUI.Box (comment2, description1[0], MD);
 		}
 		if(Button4.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[4], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[4], MD);
+			GUI.Box (comment2, description1[0], MD);
 		}
 		if(Button5.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[5], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[5], MD);
+			GUI.Box (comment2, description1[0], MD);
 		}
 		if(Button6.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[6], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[6], MD);
+			GUI.Box (comment2, description1[0], MD);
 			if(Button6BlueHalf.Contains(mousePos))
 			{
-				GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[0], MD);
-				GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[8], MD);
+				GUI.Box (comment1, description1[0], MD);
+				GUI.Box (comment2, description1[8], MD);
 			}
 			if(Button6RedHalf.Contains(mousePos))
 			{
-				GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[0], MD);
-				GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[9], MD);
+				GUI.Box (comment1, description1[0], MD);
+				GUI.Box (comment2, description1[9], MD);
 			}
 		}
 		if(Button7.Contains(mousePos))
 		{
-			GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[7], MD);
-			GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[0], MD);
+			GUI.Box (comment1, description1[7], MD);
+			GUI.Box (comment2, description1[0], MD);
 			if(Button7BlueHalf.Contains(mousePos))
 			{
-				GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[0], MD);
-				GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[8], MD);
+				GUI.Box (comment1, description1[0], MD);
+				GUI.Box (comment2, description1[8], MD);
 			}
 			if(Button7RedHalf.Contains(mousePos))
 			{
-				GUI.Box (new Rect (960f, 927f, 650f, 31f), description1[0], MD);
-				GUI.Box (new Rect (940f, 968f, 650f, 31f), description1[9], MD);
+				GUI.Box (comment1, description1[0], MD);
+				GUI.Box (comment2, description1[9], MD);
 			}
 		}
 
